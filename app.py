a0318dcf9c20f2b835c8701a482a5bbc6a27343f
dmiguelgaliana <dmiguelgaliana@gmail.com>
2026-02-02 20:48:05 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+from db import insertar_usuario, obtener_mail
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("base.html")
+
+@app.route("/getmail", methods=["GET", "POST"])
+def getmail():
+    resultado = None
+    error = None
+
+    if request.method == "POST":
+        nombre = request.form.get("nombre")
+        dato = obtener_mail(nombre)
+
+        if dato:
+            resultado = f"El correo de {nombre} es: {dato[0]}"
+        else:
+            error = f"No existe ningún usuario llamado '{nombre}'."
+
+    return render_template("getmail.html", resultado=resultado, error=error)
+
+@app.route("/addmail", methods=["GET", "POST"])
+def addmail():
+    mensaje = None
+    error = None
+
+    if request.method == "POST":
+        nombre = request.form.get("nombre")
+        mail = request.form.get("mail")
+
+        try:
+            insertar_usuario(nombre, mail)
+            mensaje = f"Usuario {nombre} añadido correctamente."
+        except:
+            error = "Error al insertar usuario."
+
+    return render_template("addmail.html", mensaje=mensaje, error=error)
+
+if __name__ == "__main__":
+    app.run(debug=True)
